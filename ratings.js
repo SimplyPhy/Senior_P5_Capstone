a475c115c0384c9293f63614a58246496d0f7b8e
SimplyPhy <SimplyPhy@users.noreply.github.com>
2016-08-04 13:48:26 -0400

    feat: finish implementing course/section/segment selection

--- ratings.js
+++ ratings.js
@@ -14,18 +14,109 @@
     return output;
   }
   // Set the date input to today's date.
-  G.$date = $(".date");
+  G.$date = $('.date');
   G.$date.val(todaysDate());
 
 
-  // Store .courses selection box
-  var $courseSelection = $(".courses");
+  // Store selection boxes
+  var $courseSelection = $('.courses'),
+      $sectionSelection,
+      $segmentSelection;
 
-  /* Fills the courses selection box with all course names
+  /* Fills the courses selection box with all course names, with the default selection empty
    */
+  $courseSelection.append(new Option('', 0));
+
   for (var i = 0; i < G.courses.length; i++) {
-    var courseName = G.courses[i].name;
-    $courseSelection.append(new Option(courseName, courseName));
+    $courseSelection.append(new Option(G.courses[i].name, i));
+  }
+
+  var courseName,
+      courseValue,
+      courseSections,
+      sectionName,
+      sectionValue,
+      sectionSegments,
+      segmentName,
+      segmentValue;
+
+  function courseSelect() {
+    $courseSelection.change(function() {
+
+      sectionName = "";
+      sectionValue = "";
+      segmentName = "";
+      segmentValue = "";
+
+      if($segmentSelection) {
+        $segmentSelection.remove();
+      }
+
+      for(var i = 0; i < G.courses.length; i++) {
+        if($(this).val() == i) {
+
+          courseName = G.courses[i].name;
+          courseValue = i;
+          courseSections = G.courses[i].sections;
+
+          if($('.sections').length === 0) {
+            $('.courses').first().after('<select name="sections" class="sections"></select>');
+            $sectionSelection = $('.sections');
+            sectionSelect();
+          }
+          $sectionSelection.empty();
+
+          $sectionSelection.append(new Option('', 0));
+          for(var j = 0; j < courseSections.length; j++) {
+            $sectionSelection.append(new Option(courseSections[j].name, j));
+          }
+        }
+      }
+    });
+  }
+  courseSelect(); // move this
+
+  function sectionSelect() {
+    $sectionSelection.change(function() {
+
+      segmentName = "";
+      segmentValue = "";
+
+      for(var i = 0; i < courseSections.length; i++) {
+        if($(this).val() == i) {
+
+          sectionName = courseSections[i].name;
+          sectionValue = i;
+          sectionSegments = courseSections[i].segments;
+
+          if($('.segments').length === 0) {
+            $('.sections').first().after('<select name="segments" class="segments"></select>');
+            $segmentSelection = $('.segments');
+            segmentSelect();
+          }
+          $segmentSelection.empty();
+
+          $segmentSelection.append(new Option('', 0));
+
+          for(var j = 0; j < sectionSegments.length; j++) {
+            $segmentSelection.append(new Option(sectionSegments[j], j));
+          }
+
+        }
+      }
+    }); // add segmentSelection function, and make it so when new course is selected, the sectionSelect box disappears and the values are reset
+  }
+
+  function segmentSelect() {
+    $segmentSelection.change(function() {
+      for(var i = 0; i < sectionSegments.length; i++) {
+        if($(this).val() == i) {
+
+          segmentName = sectionSegments[i];
+          segmentValue = i;
+        }
+      }
+    });
   }
 
 })(window);
